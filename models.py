745e68e7b406d08f3d8b7bd852d83b77781a862a
Professor833 <lalit.vd.dev@gmail.com>
2025-08-18 01:51:46 +0530

    Add Alembic configuration files and update models with phone number field

--- models.py
+++ models.py
@@ -14,6 +14,7 @@ class Users(Base):
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
     role = Column(String, default="user")
+    phone_number = Column(String(length=15), nullable=True)
 
 
 class TodoItem(Base):
